b49ce895829c189ec918b78faeb6870034b0c13f
Clemens <clemens@machelp.nl>
2020-10-24 00:18:24 +0200

    Questions in file and updated logic accordingly. :art:

--- questions/views.py
+++ questions/views.py
@@ -2,28 +2,26 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 ''' TODO:
-    1. Make multiple inputs on the view.
-        a. On submit, all this data will go to the function specified in the form component (see HTML forms).
-    2. Then write the logic in that function and add it to a file.
     3. From this function return a view that goes to the results page
         a. Pass the results as a variable (context variable) to the results page
             i. The results being: Points per specialisation and specialisation with the most points.
 '''
 
 def questionslist(request):
-    # TODO: perhaps retrieve the questions from a file.
-    #      Perhaps put the answer options statically in the html file.
-    #   If you can really find the time, try to put this in excel and retrieve it from there through maybe Panda's python.
-    questions_list = [['BDaM', 'Ben jij geïnteresseerd in het automatiseren van bepaalde processen?',
-                       ['antwoord 1', 'antwoord 2', 'antwoord 3', 'antwoord 4']],
-                      ['BDaM SE', 'Wil jij graag leren hoe je data efficiënt opslaat in een database?',
-                       ['antwoord 1', 'antwoord 2', 'antwoord 3', 'antwoord 4']],
-                      ['BDaM', 'Is analyseren van data iets wat jou aanspreekt?',
-                       ['antwoord 1', 'antwoord 2', 'antwoord 3', 'antwoord 4']],
-                      ['BDaM FICT', 'Wil jij leren onderzoek te doen naar bepaalde data om jou gestelde bewering te kunnen onderbouwen?',
-                       ['antwoord 1', 'antwoord 2', 'antwoord 3', 'antwoord 4']],
-                      ['BDaM', 'Wil jij doormiddel van opgeslagen data voorspellingen kunnen doen over toekomstige ontwikkelingen? ',
-                       ['antwoord 1', 'antwoord 2', 'antwoord 3', 'antwoord 4']]]
+    # I think in PythonAnywhere you need the path /home/project/questions.txt or something like that.
+    questions_list = []
+    question_list_number = 0
+    with open('questions.txt', 'r') as f:
+        data = f.read()
+        splitted_lines = data.split('\n')
+        for index, element in enumerate(splitted_lines):
+            if index % 2 == 0:
+                questions = element.split('~')
+                questions_list.append(questions)
+            if index % 2 != 0:
+                answers = element.split(';')
+                questions_list[question_list_number].append(answers)
+                question_list_number += 1
 
     context = {'questions': questions_list}
     return render(request, 'questionslist/questionslist.html', context)
@@ -42,7 +40,8 @@ def processing_answers(request):
             continue
 
         # Points logic
-        # TODO: if answer is not acknowledigng the specialisation, then do NOT add +1
+        # TODO: if answer is not acknowledigng the specialisation, then do NOT add +1.
+        #   You can only do this if you have the answers ~_~
         if 'BDaM' in element[1][0]:
             BDaM_points += 1
         if 'FICT' in element[1][0]:
@@ -60,7 +59,7 @@ def processing_answers(request):
             f.write(formatted_answer)
 
     with open('results.txt', 'a') as f:
-        points = f'BDaM punten: {BDaM_points}\nFICT punten: {FICT_points}\nIaT punten: {IaT_points}\nSE punten: {SE_points}'
+        points = f'BDaM punten: {BDaM_points}\nFICT punten: {FICT_points}\nIaT punten: {IaT_points}\nSE punten: {SE_points}\n'
         f.write(points)
 
     return HttpResponseRedirect('/resultaten/')
