de185ed66fadeaadb1573325148fe73685e6da64
Clemens <clemens@machelp.nl>
2020-10-30 16:27:50 +0100

    Remove unused code :art:

--- questions/views.py
+++ questions/views.py
@@ -65,16 +65,8 @@ def processing_answers(request, username=no_username_backup_id):
             if answer_id == '1':
                 SE_points += 1
 
-        ## Add remaining elements, which are the answers, to the database (file).
-        # question_number = element[1][0]
-        # input_answer = element[1][1]
-        # formatted_answer = f'Vraag {question_number}:\n\t{input_answer}\n'
-        # with open(f'results/results{username}.txt', 'a') as f:
-        #     f.write(formatted_answer)
-
     with open(f'results/results{username}.txt', 'a') as f:
         points = f'{BDaM_points}:{FICT_points}:{IaT_points}:{SE_points}'
-        # points = f'BDaM punten: {BDaM_points}\nFICT punten: {FICT_points}\nIaT punten: {IaT_points}\nSE punten: {SE_points}\n'
         f.write(points)
 
     return HttpResponseRedirect(f'/resultaten/{username}/')
